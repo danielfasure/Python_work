8cdabfc0de344f6067484040e76c1fa5998175a9
danielfasure <danielfasure@gmail.com>
2026-01-19 15:52:37 +0000

    finishing tuples

---
+++ basic_python/Tuples.py
@@ -0,0 +1,11 @@
+"""
+tuples
+Key characteristics
+- Ordered: The items in a tuple maintain their position.
+- Immutable: Once created, you cannot change, add, or remove elements.
+- Heterogeneous: They can hold different data types (strings, numbers, objects, etc.).
+- Indexed: You access elements by their position (starting at 0).
+
+"""
+my_tuple = (1,2,3,5,6,7)
+#here the values inside this tuple cant be changed or increased or decreased
