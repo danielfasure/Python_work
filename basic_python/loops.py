1d6245507e3c8d15ec2a5d3f8b882858b323ff7a
danielfasure <danielfasure@gmail.com>
2026-01-22 13:30:37 +0000

    finishing if else

---
+++ basic_python/loops.py
@@ -0,0 +1,16 @@
+#for loops
+#using for loops
+my_list = [1,2,3,4,5,6,7,8,9]
+
+for x in my_list:
+    print(x)
+#using range for python for loops
+for x in range(0,len(my_list)):
+    print(my_list[x])
+
+#using while using x
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i = i + 1
+
