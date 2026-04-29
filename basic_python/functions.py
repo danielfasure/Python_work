0e4cbb3ab92d1dc14e24a9a241f7fbd541fcaecd
danielfasure <danielfasure@gmail.com>
2026-04-29 03:45:22 +0100

    added function.py

---
+++ basic_python/functions.py
@@ -0,0 +1,12 @@
+"""
+functions are resuable pieces of code used to perform a task when called apon 
+
+def is the keywords used to define them and to call the function (name_of_function())
+
+
+"""
+
+def first_function():
+    print("inside the function yooo")
+
+my_function()    
