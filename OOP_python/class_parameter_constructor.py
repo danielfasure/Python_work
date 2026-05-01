22e41363bb6de327ec478fcd2da51629c5dbbb2d
danielfasure <danielfasure@gmail.com>
2026-05-01 01:29:59 +0100

    constructor added

---
+++ OOP_python/class_parameter_constructor.py
@@ -0,0 +1,12 @@
+class daniel_user_parmeters:
+
+
+
+    def __init__(self,firstname,lastname,age,email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.email = email
+
+    def talk(self):
+        print("the daniel class called here has been store in main ")
--- OOP_python/main.py
+++ OOP_python/main.py
@@ -1,4 +1,10 @@
 from oop_user_class import *
+from class_parameter_constructor import *
+
 
 main_user = daniel_user()
 print(main_user.name)
+
+daniel = daniel_user_parmeters("daniel","fasure",25,"danielfasure@Gmail.com")
+print(daniel.firstname)
+daniel.talk()
