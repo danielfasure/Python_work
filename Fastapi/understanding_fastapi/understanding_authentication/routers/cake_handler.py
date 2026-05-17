e1cb437a91abd3669402a0f0d04ca3432cb45627
danielfasure <danielfasure@gmail.com>
2026-05-17 18:00:23 +0100

    forgot to hit save on visual studio code on pc

--- Fastapi/understanding_fastapi/understanding_authentication/routers/cake_handler.py
+++ Fastapi/understanding_fastapi/understanding_authentication/routers/cake_handler.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from models.models import *
+
 
 router = APIRouter()
 
--- Fastapi/understanding_fastapi/understanding_authentication/routers/user_handler.py
+++ Fastapi/understanding_fastapi/understanding_authentication/routers/user_handler.py
@@ -1,18 +1,23 @@
-from urllib.request import Request
 
-from fastapi import APIRouter
+
+from fastapi import APIRouter,HTTPException
 from models.models import *
 from models.models_valdations import *
+from passlib.context import CryptContext
+
 
 
 router = APIRouter()
+
+brcrypt_context =CryptContext(schemes=['bcrypt'],deprecated='auto') #setting the alogrithim used to hash password 
 @router.post("/login")
 async def create_user(user: user_validate):
     created_user = User(username=user.username,
                         email=user.email,
                         has_paid=user.has_paid,
                         role=user.role,
-                        hash_password=user.password)
+
+                        hash_password=brcrypt_context.hash(user.password))# using it to pass hash password 
     return created_user
 
 
